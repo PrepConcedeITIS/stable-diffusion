3cc7ac6262bf029264586c678a8fff0234447a3a
Tagmir Gilyazov <44526727+PrepConcedeITIS@users.noreply.github.com>
2022-09-29 22:03:52 +0300

    txt-to-img web adapter

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -17,6 +17,39 @@ from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
 
 
+class Input:
+    def __init__(self,
+                 prompt,
+                 scale,
+                 outdir,
+                 ddim_steps,
+                 n_iter,
+                 seed):
+        self.prompt = prompt
+        self.scale = scale
+        self.outdir = outdir
+        self.ddim_steps = ddim_steps
+        self.n_iter = n_iter
+        self.seed = seed
+        self.fp16 = True
+        self.skip_grid = True
+        self.skip_save = False
+        self.plms = False
+        self.laion400m = False
+        self.config = "../configs/stable-diffusion/v1-inference.yaml"
+        self.ckpt = "../models/ldm/stable-diffusion-v1/model.ckpt"
+        self.ddim_eta = 0.0
+        self.W = 512
+        self.H = 512
+        self.C = 4
+        self.f = 8
+        self.n_samples = 1
+        self.n_rows = 0
+        self.from_file = False
+        self.precision = "autocast"
+        self.fixed_code = False
+
+
 def chunk(it, size):
     it = iter(it)
     return iter(lambda: tuple(islice(it, size)), ())
@@ -42,146 +75,152 @@ def load_model_from_config(config, ckpt, verbose=False):
     return model
 
 
-def main():
-    parser = argparse.ArgumentParser()
+def main(generation_params: Input = None):
+    opt = None
+    if generation_params is None:
 
-    parser.add_argument(
-        "--prompt",
-        type=str,
-        nargs="?",
-        default="a painting of a virus monster playing guitar",
-        help="the prompt to render"
-    )
-    parser.add_argument(
-        "--outdir",
-        type=str,
-        nargs="?",
-        help="dir to write results to",
-        default="outputs/txt2img-samples"
-    )
-    parser.add_argument(
-        "--skip_grid",
-        action='store_true',
-        help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
-    )
-    parser.add_argument(
-        "--skip_save",
-        action='store_true',
-        help="do not save individual samples. For speed measurements.",
-    )
-    parser.add_argument(
-        "--ddim_steps",
-        type=int,
-        default=50,
-        help="number of ddim sampling steps",
-    )
-    parser.add_argument(
-        "--plms",
-        action='store_true',
-        help="use plms sampling",
-    )
-    parser.add_argument(
-        "--laion400m",
-        action='store_true',
-        help="uses the LAION400M model",
-    )
-    parser.add_argument(
-        "--fixed_code",
-        action='store_true',
-        help="if enabled, uses the same starting code across samples ",
-    )
-    parser.add_argument(
-        "--ddim_eta",
-        type=float,
-        default=0.0,
-        help="ddim eta (eta=0.0 corresponds to deterministic sampling",
-    )
-    parser.add_argument(
-        "--n_iter",
-        type=int,
-        default=2,
-        help="sample this often",
-    )
-    parser.add_argument(
-        "--H",
-        type=int,
-        default=512,
-        help="image height, in pixel space",
-    )
-    parser.add_argument(
-        "--W",
-        type=int,
-        default=512,
-        help="image width, in pixel space",
-    )
-    parser.add_argument(
-        "--C",
-        type=int,
-        default=4,
-        help="latent channels",
-    )
-    parser.add_argument(
-        "--f",
-        type=int,
-        default=8,
-        help="downsampling factor",
-    )
-    parser.add_argument(
-        "--n_samples",
-        type=int,
-        default=3,
-        help="how many samples to produce for each given prompt. A.k.a. batch size",
-    )
-    parser.add_argument(
-        "--n_rows",
-        type=int,
-        default=0,
-        help="rows in the grid (default: n_samples)",
-    )
-    parser.add_argument(
-        "--scale",
-        type=float,
-        default=7.5,
-        help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
-    )
-    parser.add_argument(
-        "--from-file",
-        type=str,
-        help="if specified, load prompts from this file",
-    )
-    parser.add_argument(
-        "--config",
-        type=str,
-        default="configs/stable-diffusion/v1-inference.yaml",
-        help="path to config which constructs model",
-    )
-    parser.add_argument(
-        "--ckpt",
-        type=str,
-        default="models/ldm/stable-diffusion-v1/model.ckpt",
-        help="path to checkpoint of model",
-    )
-    parser.add_argument(
-        "--seed",
-        type=int,
-        default=42,
-        help="the seed (for reproducible sampling)",
-    )
-    parser.add_argument(
-        "--precision",
-        type=str,
-        help="evaluate at this precision",
-        choices=["full", "autocast"],
-        default="autocast"
-    )
-    parser.add_argument(
-        "--fp16",
-        action='store_true',
-        help="before running inference, convert the model to fp16 "
-             "(this saves runtime memory, though at a potential loss of some quality)",
-        default=False
-    )
-    opt = parser.parse_args()
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            "--prompt",
+            type=str,
+            nargs="?",
+            default="a painting of a virus monster playing guitar",
+            help="the prompt to render"
+        )
+        parser.add_argument(
+            "--outdir",
+            type=str,
+            nargs="?",
+            help="dir to write results to",
+            default="outputs/txt2img-samples"
+        )
+        parser.add_argument(
+            "--skip_grid",
+            action='store_true',
+            help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
+        )
+        parser.add_argument(
+            "--skip_save",
+            action='store_true',
+            help="do not save individual samples. For speed measurements.",
+        )
+        parser.add_argument(
+            "--ddim_steps",
+            type=int,
+            default=50,
+            help="number of ddim sampling steps",
+        )
+        parser.add_argument(
+            "--plms",
+            action='store_true',
+            help="use plms sampling",
+        )
+        parser.add_argument(
+            "--laion400m",
+            action='store_true',
+            help="uses the LAION400M model",
+        )
+        parser.add_argument(
+            "--fixed_code",
+            action='store_true',
+            help="if enabled, uses the same starting code across samples ",
+        )
+        parser.add_argument(
+            "--ddim_eta",
+            type=float,
+            default=0.0,
+            help="ddim eta (eta=0.0 corresponds to deterministic sampling",
+        )
+        parser.add_argument(
+            "--n_iter",
+            type=int,
+            default=2,
+            help="sample this often",
+        )
+        parser.add_argument(
+            "--H",
+            type=int,
+            default=512,
+            help="image height, in pixel space",
+        )
+        parser.add_argument(
+            "--W",
+            type=int,
+            default=512,
+            help="image width, in pixel space",
+        )
+        parser.add_argument(
+            "--C",
+            type=int,
+            default=4,
+            help="latent channels",
+        )
+        parser.add_argument(
+            "--f",
+            type=int,
+            default=8,
+            help="downsampling factor",
+        )
+        parser.add_argument(
+            "--n_samples",
+            type=int,
+            default=3,
+            help="how many samples to produce for each given prompt. A.k.a. batch size",
+        )
+        parser.add_argument(
+            "--n_rows",
+            type=int,
+            default=0,
+            help="rows in the grid (default: n_samples)",
+        )
+        parser.add_argument(
+            "--scale",
+            type=float,
+            default=7.5,
+            help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+        )
+        parser.add_argument(
+            "--from-file",
+            type=str,
+            help="if specified, load prompts from this file",
+        )
+        parser.add_argument(
+            "--config",
+            type=str,
+            default="configs/stable-diffusion/v1-inference.yaml",
+            help="path to config which constructs model",
+        )
+        parser.add_argument(
+            "--ckpt",
+            type=str,
+            default="models/ldm/stable-diffusion-v1/model.ckpt",
+            help="path to checkpoint of model",
+        )
+        parser.add_argument(
+            "--seed",
+            type=int,
+            default=42,
+            help="the seed (for reproducible sampling)",
+        )
+        parser.add_argument(
+            "--precision",
+            type=str,
+            help="evaluate at this precision",
+            choices=["full", "autocast"],
+            default="autocast"
+        )
+        parser.add_argument(
+            "--fp16",
+            action='store_true',
+            help="before running inference, convert the model to fp16 "
+                 "(this saves runtime memory, though at a potential loss of some quality)",
+            default=False
+        )
+        opt = parser.parse_args()
+
+    else:
+        opt = generation_params
 
     if opt.laion400m:
         print("Falling back to LAION 400M model...")
@@ -230,7 +269,7 @@ def main():
     if opt.fixed_code:
         start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
 
-    precision_scope = autocast if opt.precision=="autocast" else nullcontext
+    precision_scope = autocast if opt.precision == "autocast" else nullcontext
     with torch.no_grad():
         with precision_scope("cuda"):
             with model.ema_scope():
---
+++ web/api.py
@@ -0,0 +1,24 @@
+import os
+import glob
+import random
+from flask import Flask, request, jsonify, send_file
+from scripts.txt2img import Input, main
+
+app = Flask(__name__)
+
+
+@app.route("/text-to-image", methods=['POST'])
+def make_predict():
+    json = request.get_json()
+    prompt = json['prompt']
+    seed = random.randint(100000000, 999999999)
+    params = Input(prompt, 12, "outputs/txt2img-samples", 25, 1, seed)
+    main(params)
+    list_of_files = glob.glob('outputs/txt2img-samples/samples/*.png')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return send_file(path_or_file=latest_file,
+                     download_name=f"{prompt}.png",
+                     mimetype="image/png")
+
+
+app.run(host='localhost', port=14100, debug=True)
